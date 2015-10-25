fbfb68b1bde57e4ac0d13fa5e161238783781556
Sergej Birklin <sergej.birklin@skillworks.de>
2015-10-26 00:03:37 +0100

    hello Spielerei entfertnt und Game als JAR eingefügt

--- 2PN_Activator/app/controllers/Application.java
+++ 2PN_Activator/app/controllers/Application.java
@@ -2,13 +2,15 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import de.htwg.se.tpn.controller.TpnController;
 import views.html.*;
 
 public class Application extends Controller {
 
+    TpnController helloController = new TpnController(1, 1);
+
     public Result index() {
-        return ok(index.render("Die Applikation wurde gestartet."));
+        return ok(index.render("Nico ist cool."));
     }
 
 }
