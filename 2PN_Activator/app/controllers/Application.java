46fe750893cb61634403ce6efae85ea1a61a9fe3
Sergej Birklin <sergej.birklin@skillworks.de>
2015-11-03 21:42:20 +0100

    Singleton Controller in Application angepasst. Gui und Tui werden allerdings aufgerufen

--- 2PN_Activator/app/controllers/Application.java
+++ 2PN_Activator/app/controllers/Application.java
@@ -2,12 +2,19 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import de.htwg.se.tpn.TwoPN;
 import de.htwg.se.tpn.controller.TpnController;
+import de.htwg.se.tpn.controller.TpnControllerInterface;
+
 import views.html.*;
 
 public class Application extends Controller {
 
-    TpnController controller = new TpnController(5, 2);
+    // static ISudokuController controller = Sudoku.getInstance().getController();
+
+    static TpnControllerInterface controller = TwoPN.getInstance().getController();
+
+    //TpnController controller = new TpnController(5, 2);
 
 
     public Result index() {
